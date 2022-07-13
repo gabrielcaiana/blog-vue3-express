899ac4a5615f14ddedbee5ff77492983e5fdb1dc
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-07-12 22:12:48 -0300

    chore: create type period

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+  import { ref } from 'vue'
+
+  const periods = ['Today', 'This week', 'This month'] as const;
+  
+  type Period = typeof periods[number]
+
+  const selectedPeriod = ref<Period>('Today')
+
+  const selectPeriod = (period: Period) => {
+    selectedPeriod.value = period
+  }
 </script>
 
 <template>
   <nav class="is-primary panel">
     <span class="panel-tabs">
-    <a>Today</a> 
-    <a>This week</a>
-    <a>This Month</a>
+    <a 
+      v-for="period in periods"
+      :key="period"
+       @click="selectPeriod(period)"
+      :class="{ 'is-active': period === selectedPeriod}">
+      {{ period }}
+    </a> 
     </span>
   </nav>
 </template>
