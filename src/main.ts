5ee4cc6987429a22ad1bda30cc9bee8baa70b9dc
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-07-13 00:11:12 -0300

    install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 import 'bulma/css/bulma.min.css'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(createPinia())
+.mount('#app')
---
+++ src/stores/posts.ts
@@ -0,0 +1,5 @@
+ import { defineStore } from 'pinia'
+
+ export const usePosts = defineStore("posts", {
+
+ })
