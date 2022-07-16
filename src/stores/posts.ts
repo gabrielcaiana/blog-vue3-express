85872f77ab4c2ada5d55443f8c7f0341cdc97d6d
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-07-16 15:20:03 -0300

    🚧  init post state

--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,5 +1,20 @@
  import { defineStore } from 'pinia'
+ import { Post, today, thisWeek, thisMonth } from '../interfaces/posts'
+
+interface PostState {
+  ids: string[]
+  all: Map<string, Post>
+ }
 
  export const usePosts = defineStore("posts", {
+  state: (): PostState => ({
+    ids: [today.id, thisWeek.id, thisMonth.id],
+    all: new Map([
+      [today.id, today],
+      [thisWeek.id, thisWeek],
+      [thisMonth.id, thisMonth]
+    ])
+  }),
 
+  actions: {}
  })
