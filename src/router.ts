abf4394fc4962329d556b6afdb822711a544e0de
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-07-16 18:28:12 -0300

    ✨ feat: create page not found

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,11 @@ export const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: () => import("./views/NotFound.vue"),
+    },
+    {
       path: '/',
       name: 'home',
       component: () => import('./views/Home.vue')
---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h2>Oooops!</h2>
+    <p>Page not found!</p>
+    <router-link to="/"> return to home</router-link>
+  </div>
+</template>
