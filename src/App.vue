7bbaf123a5c9a9ce2e1880a3e3818ab3efabc0f0
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-07-16 18:23:30 -0300

    ✨ feat: create routes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,8 @@
-<script setup lang="ts">
-import Timeline from './components/Timeline.vue'
-</script>
-
 <template>
   <main>
     <div class="section">
       <div class="container">
-      <Suspense>
-       <template #default>
-        <Timeline />
-       </template>
-
-       <template #fallback>
-        <progress class="progress is-primary is-small" />
-       </template>
-      </Suspense>
+        <RouterView />
       </div>
     </div>
   </main>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import { router } from './router'
 import 'bulma/css/bulma.min.css'
 
 createApp(App)
 .use(createPinia())
+.use(router)
 .mount('#app')
