2edbb6c5bf5d3dd25aa4efb8994d590374cb3dd0
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-07-17 18:00:30 -0300

    ✨ feat: create composable modal

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <script lang="ts" setup>
   import Navbar from './components/Navbar.vue';
+  import { useModal } from './composables/modal'
+  import { computed } from 'vue'
+
+  const modal = useModal()
+  const modalStyle = computed(() => {
+    return {
+      display: modal.show.value ? 'block' : 'none',
+    }
+  })
 </script>
 
 <template>
-  <main>
-    <div class="section">
-      <div class="container">
-        <Navbar />
-        <RouterView />
+  <div class="modal" style="color: white;" :style="modalStyle">
+    <div class="modal-background">
+      <div class="modal-content">
+        <div id="modal"></div>
       </div>
     </div>
-  </main>
+    <button @click="modal.hideModal()" class="modal-close is-large"></button>
+  </div>
+
+  <div class="section">
+    <div class="container">
+      <Navbar />
+      <RouterView />
+    </div>
+  </div>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,21 @@
+<script lang="ts" setup>
+  import { useModal } from '../composables/modal'
+
+  const modal = useModal()
+
+</script>
+
 <template>
   <div class="navbar">
     <div class="navbar-end">
       <div class="buttons">
+        <button class="button" @click="modal.showModal()">Sign Up</button>
         <router-link to="/posts/new" class="button">New Post</router-link>
       </div>
     </div>
   </div>
+
+  <Teleport to="#modal">
+    Content to be rendered in the modal
+  </Teleport>
 </template>
