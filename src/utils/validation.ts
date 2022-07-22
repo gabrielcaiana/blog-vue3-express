33dbc70aedfe3e1b24c82a01a04a9f249aed43f4
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-07-22 07:24:07 -0300

    ✨ feat: add validations

---
+++ src/utils/validation.ts
@@ -0,0 +1,40 @@
+interface Status {
+  valid: boolean
+  message?: string
+}
+
+type Rule = (value: string) => Status
+
+export const length = ({ min, max }: {min: number, max: number}): Rule => {
+  return function (value: string): Status {
+    const result = Boolean(value.length >= min && value.length <= max)
+
+    return {
+      valid: result,
+      message: result ? undefined : `Must be between ${min} and ${max} characters`
+    }
+  }
+}
+
+export const required: Rule = (value: string): Status => {
+  const result = Boolean(value)
+
+  return {
+    valid: result,
+    message: result ? undefined : 'This field is required',
+  }
+}
+
+export const validate = (value: string, rules: Rule[]): Status =>  {
+  for(const rule of rules) {
+    const result = rule(value)
+
+    if(!result.valid) {
+      return result
+    }
+  }
+
+  return {
+    valid: true
+  }
+}
