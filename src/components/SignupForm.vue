ba092be237e8862e5a340691ca63230e3872def9
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-08-15 16:39:58 -0300

    chore: validate modal login

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
   import FormIput from './FormIput.vue';
   import { validate, length, required } from '../utils/validation'
+  import { NewUser } from '../interfaces/user'
   import { computed, ref } from 'vue'
 
   const username = ref('')
@@ -8,13 +9,27 @@
 
   const password = ref('')
   const passwordStatus = computed(() => validate(password.value, [required, length({min: 10, max: 20})]))
+
+  const isInvalid = computed(() => !usernameStatus.value.valid || !passwordStatus.value.valid)
+
+  const handleSubmit = () => {
+    if(isInvalid.value) return
+
+    const newUser: NewUser = {
+       username: username.value,
+      password: password.value
+    }
+
+    console.log(newUser)
+  }
 </script>
 
 <template>
-  <div class="form">
+  <form class="form" @submit.prevent="handleSubmit">
     <FormIput name="Username" v-model="username" :status="usernameStatus" />
     <FormIput name="Password" v-model="password" :status="passwordStatus" />
-  </div>
+    <button type="submit" class="button" :disabled="isInvalid">Submit</button>
+  </form>
 </template>
 
 <style lang="css" scoped>
