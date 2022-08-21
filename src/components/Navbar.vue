40910d2a36cf015d493a9e96d309e0838d3e14e3
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-08-21 16:51:46 -0300

    chore: create dinamic component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,33 @@
 <script lang="ts" setup>
-  import { useModal } from '~/composables/modal'
-  import { useUsers } from '~/stores/users';
-  import SignupForm from './SignupForm.vue';
+import { useModal } from '~/composables/modal';
+import { useUsers } from '~/stores/users';
 
-  const modal = useModal()
-  const usersStore = useUsers()
+const modal = useModal();
+const usersStore = useUsers();
 </script>
 
 <template>
   <div class="navbar">
     <div class="navbar-end">
       <div v-if="usersStore.currentUserId" class="buttons">
-        <router-link :to="{ name: 'new-post'}" class="button">New Post</router-link>
+        <router-link :to="{ name: 'new-post' }" class="button"
+          >New Post</router-link
+        >
         <button class="button" @click="usersStore.logout()">Log Out</button>
       </div>
 
       <div v-else class="buttons">
-        <button class="button" @click="modal.showModal()">Sign Up</button>
-        <RouterLink to="/posts/new" class="button">Sign In</RouterLink>
+        <button class="button" @click="modal.showModal('signUp')">
+          Sign Up
+        </button>
+        <button class="button" @click="modal.showModal('signIn')">
+          Sign In
+        </button>
       </div>
     </div>
   </div>
 
   <Teleport to="#modal">
-    <SignupForm />
+    <component :is="modal.component.value" />
   </Teleport>
 </template>
--- src/composables/modal.ts
+++ src/composables/modal.ts
@@ -1,11 +1,25 @@
-import { ref } from 'vue'
+import { ref, shallowRef } from 'vue';
 
-const show = ref(false)
+import SignupForm from '~/components/SignupForm.vue';
+import Timeline from '~/components/Timeline.vue';
+
+const show = ref(false);
+const component = shallowRef();
 
 export const useModal = () => {
   return {
     show,
-    showModal: () => show.value = true,
-    hideModal: () => show.value = false,
-  }
-}
+    component,
+    showModal: (type: 'signIn' | 'signUp') => {
+      show.value = true;
+
+      switch (type) {
+        case 'signUp':
+          return (component.value = SignupForm);
+        case 'signIn':
+          return (component.value = Timeline);
+      }
+    },
+    hideModal: () => (show.value = false),
+  };
+};
